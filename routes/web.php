c30094bb53464a752be7cacd277bd5099cdba2e5
DavideAlovisio13 <alovisiod@gmail.com>
2024-06-05 14:02:16 +0200

    (prompt, route) genearte msrc + model Type and add routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProjectController;
-
+use App\Http\Controllers\Admin\TypeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +24,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::middleware(['auth'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class)->parameter('projects', 'project:slug');
+    Route::resource('types', TypeController::class)->parameter('types', 'type:slug');
 });
 
 // group: raggruppa tutte le rotte che posso avere qualcosa in comune
