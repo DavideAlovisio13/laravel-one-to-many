b80a428ba9b03dfdcfa5d868c1c0dde97a690d27
DavideAlovisio13 <alovisiod@gmail.com>
2024-06-05 14:25:29 +0200

    (migrations, Type) add foreign key and modify Type model

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
+use Illuminate\Support\Str;
 
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable = ['data_science_projects', 'web-design_projects', 'ui-ux_projects', 'mobile_projects', 'game_projects', 'machine_learning_projects', 'other_projects'];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public static function generateSlug($name)
+    {
+        $slug = Str::slug($name, '-');
+        $count = 1;
+        while (Type::where('slug', $slug)->first()) {
+            $slug = Str::of($name)->slug('-') . "-{$count}";
+            $count++;
+        }
+        return $slug;
+    }
 }
